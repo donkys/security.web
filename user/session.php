07495a7597acd1b02f779c9b2a1c189d178d7eee
donkys <63050156@kmitl.ac.th>
2023-04-23 15:21:54 +0700

    add project

---
+++ user/session.php
@@ -0,0 +1,7 @@
+<?php
+    session_start();
+    if ($_SESSION['key']!="user"){
+        header('Location: ./security.porapipat.me/index.php');
+    }
+    echo "<center><a href='./security.porapipat.me/index.php'><button type='submit' width=100px height=30px><font size=7>LOG OUT</font></button></a></center><br>";
+?>
