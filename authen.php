171132f2801cc85ff7db9e29bb12b43f0d667289
donkys <63050156@kmitl.ac.th>
2023-04-24 22:18:37 +0700

    add some bug for pentest

--- authen.php
+++ authen.php
@@ -13,37 +13,25 @@
                     E_Role,
                     E_Address
                 FROM pts_employee WHERE E_Username='$username' AND (E_Password = '$password');";
-    $result = $conn->query($showdata);
+    // $result = $conn->real_query($showdata);
+    $result = $conn->multi_query($showdata);
+    $result = $conn->store_result();
+    $row = $result->fetch_assoc();
+    print($row["E_ID"]);
+        
     $conn->close();
-
+    // $a = $result->mysqli_result();
     $rowcount = mysqli_num_rows($result);
 
-    $row = $result->fetch_assoc();
-
-    // if($rowcount == 1){
-
-    //     $_SESSION['id']=$row["E_ID"];
-    //     $_SESSION['name']=$row["E_Name"];
-    //     $_SESSION['surname']=$row["E_Surname"];
-    //     $_SESSION['key']=$row["E_Role"];
-    //     $_SESSION['address']=$row["E_Address"];
-        
-    //     if($_SESSION['key'] == "admin"){
-    //         header("Location: ./operator/index.php");
-    //     }else {
-    //         header("Location: ./user/index.php");
-    //     }
-    // }else {
-    //     header("Location: index.php");
-    // }
+    // $row = $result->fetch_assoc();
 
     if($rowcount == 1){
 
-        $_SESSION['id']=101;
-        $_SESSION['name']="John";
-        $_SESSION['surname']="Admin";
-        $_SESSION['key']="admin";
-        $_SESSION['address']="amhome";
+        $_SESSION['id']=$row["E_ID"];
+        $_SESSION['name']=$row["E_Name"];
+        $_SESSION['surname']=$row["E_Surname"];
+        $_SESSION['key']=$row["E_Role"];
+        $_SESSION['address']=$row["E_Address"];
         
         if($_SESSION['key'] == "admin"){
             header("Location: ./operator/index.php");
@@ -53,4 +41,21 @@
     }else {
         header("Location: index.php");
     }
+
+    // if($rowcount == 1){
+
+    //     $_SESSION['id']=101;
+    //     $_SESSION['name']="John";
+    //     $_SESSION['surname']="Admin";
+    //     $_SESSION['key']="admin";
+    //     $_SESSION['address']="amhome";
+        
+    //     if($_SESSION['key'] == "admin"){
+    //         header("Location: ./operator/index.php");
+    //     }else {
+    //         header("Location: ./user/index.php");
+    //     }
+    // }else {
+    //     header("Location: index.php");
+    // }
 ?>
--- user/session.php
+++ user/session.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if ($_SESSION['key']!="user"){
-        header('Location: ./security.porapipat.me/index.php');
+        header('Location: ../index.php');
     }
-    echo "<center><a href='./security.porapipat.me/index.php'><button type='submit' width=100px height=30px><font size=7>LOG OUT</font></button></a></center><br>";
+    echo "<center><a href='../index.php'><button type='submit' width=100px height=30px><font size=7>LOG OUT</font></button></a></center><br>";
 ?>
